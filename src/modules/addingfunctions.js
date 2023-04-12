600b9c4a2be67608f1a16eba642a117198486ad9
tsheporamantso <tshepo.ramantso@outlook.com>
2023-04-12 14:00:10 +0200

    Add event listener on refresh and submit buttons

--- src/modules/addingfunctions.js
+++ src/modules/addingfunctions.js
@@ -3,8 +3,26 @@ import ScoreBoard from './scoreboard.js';
 const scoreBoard = new ScoreBoard();
 
 const scoreForm = document.getElementById('scoreForm');
-scoreForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  scoreBoard.addNewScore();
+
+const formFunction = () => {
+  scoreForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const scoreName = document.querySelector('.scoreName');
+    const score = document.querySelector('.score');
+    if (scoreName.value !== '' && score.value !== '') {
+      scoreBoard.addNewScore(scoreName.value, score.value);
+    }
+    scoreName.value = '';
+    score.value = '';
+  });
   scoreBoard.displayScore();
-});
+};
+
+const refresh = document.querySelector('.refresh');
+const refreshFunction = () => {
+  refresh.addEventListener('click', () => {
+    window.location.reload();
+    scoreBoard.displayScore();
+  });
+};
+export { formFunction, refreshFunction };
