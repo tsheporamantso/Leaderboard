089bf9b2686b6a2e0ef16e8ec088c5c30473cb5c
tsheporamantso <tshepo.ramantso@outlook.com>
2023-04-12 13:57:51 +0200

    Import takingid.js, call form and refresh functions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './style.css';
-import './modules/addingfunctions.js';
+// import './modules/addingfunction.js';
+import takingId from './modules/takingId.js';
+// import ScoreBoard from './modules/scoreboard.js';
+import { formFunction, refreshFunction } from './modules/addingfunctions.js';
+
+takingId();
+
+formFunction();
+refreshFunction();
